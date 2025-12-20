faff204ac405f203b39ae8949b4f0574d61092e9
Elijah <elijah.widenerferreira@gmail.com>
2025-12-19 22:55:17 -0500

    Provisioned DBs

--- point-game-infra/lib/point-game-infra-stack.ts
+++ point-game-infra/lib/point-game-infra-stack.ts
@@ -1,11 +1,116 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import {Construct} from 'constructs';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PointGameInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // DynamoDB tables
+    new dynamodb.Table(this, 'Users', {
+      tableName: 'Users',
+      partitionKey: {
+        name: 'userID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'GameTables', {
+      tableName: 'GameTables',
+      partitionKey: {
+        name: 'tableID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'GameState', {
+      tableName: 'GameState',
+      partitionKey: {
+        name: 'tableID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'InterRoundActionQueue', {
+      tableName: 'InterRoundActionQueue',
+      partitionKey: {
+        name: 'tableID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'actionSeq',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'Ledger', {
+      tableName: 'Ledger',
+      partitionKey: {
+        name: 'tableID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'ledgerSeq',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'ConnectionStore', {
+      tableName: 'ConnectionStore',
+      partitionKey: {
+        name: 'tableID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'connectionID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'HandSnapshots', {
+      tableName: 'HandSnapshots',
+      partitionKey: {
+        name: 'tableID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'handSeq',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'ActionLog', {
+      tableName: 'ActionLog',
+      partitionKey: {
+        name: 'handID',  // ${tableID}#${handSeq}
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'actionSeq',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new dynamodb.Table(this, 'Timers', {
+      tableName: 'Timers',
+      partitionKey: {
+        name: 'tableID',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'timerSeq',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
   }
 }
