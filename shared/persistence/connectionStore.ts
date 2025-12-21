7f6d2523a46b842b2ade0cec7c60c787ff403513
Elijah <elijah.widenerferreira@gmail.com>
2025-12-21 13:47:32 -0500

    connection store persistence layer

--- shared/persistence/connectionStore.ts
+++ shared/persistence/connectionStore.ts
@@ -1,5 +1,5 @@
-import {GetItemCommand} from '@aws-sdk/client-dynamodb';
-import {unmarshall} from '@aws-sdk/util-dynamodb';
+import {DeleteItemCommand, GetItemCommand, PutItemCommand} from '@aws-sdk/client-dynamodb';
+import {marshall, unmarshall} from '@aws-sdk/util-dynamodb';
 
 import {ddb} from './dynamo/client';
 import {TABLES} from './dynamo/tables';
@@ -18,4 +18,27 @@ export async function loadConnectionStore(
     return null;
   }
   return unmarshall(result.Item) as ConnectionStore;
+}
+
+export async function registerConnection(
+    tableID: string, connectionID: string, playerID: string): Promise<void> {
+  const item: ConnectionStore = {tableID, connectionID, playerID};
+
+  await ddb.send(new PutItemCommand({
+    TableName: TABLES.CONNECTION_STORE,
+    Item: marshall(item),
+    ConditionExpression:
+        'attribute_not_exists(tableID) AND attribute_not_exists(connectionID)'
+  }));
+}
+
+export async function removeConnection(
+    tableID: string, connectionID: string): Promise<void> {
+  await ddb.sent(new DeleteItemCommand({
+    TableName: TABLES.CONNECTION_STORE,
+    Key: {
+      tableID: {S: tableID},
+      connectionID: {S: connectionID},
+    },
+  }));
 }
